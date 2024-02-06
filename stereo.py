ec1244405410c4b5e1db3c9853b7603c34f2c33f
Chtun <hchitsein@gmail.com>
2024-02-05 19:22:36 -0600

    Added soft NMS

--- stereo.py
+++ stereo.py
@@ -18,7 +18,7 @@ from tqdm import tqdm
 from draw_boxes import draw_bounding_box
 
 
-def map_to_sphere(x, y, z, radius, yaw_radian, pitch_radian):
+def map_to_sphere(x, y, z, radius, yaw_radian, pitch_radian, normalizing=True):
     
     '''
     Function:
@@ -64,7 +64,8 @@ def map_to_sphere(x, y, z, radius, yaw_radian, pitch_radian):
                            np.cos(theta) * np.sin(pitch_radian),
                            np.sin(theta) * np.cos(phi))
     phi_prime += yaw_radian
-    phi_prime = phi_prime % (2 * np.pi)
+    if normalizing:
+        phi_prime = phi_prime % (2 * np.pi)
 
     return theta_prime.flatten(), phi_prime.flatten()
 
@@ -217,7 +218,106 @@ def panorama_to_stereo_multiprojections(panorama_path, stereographic_image_size,
 
     return frames_with_meta_np
 
+def calculate_IoU(box1, box2):
+    '''
+    Function:
+        Take two boxes and calculate the intersection-over-union value
+    
+    Input:
+        box1 (int/float, int/float, int/float, int/float): (x, y, w, h) of first box
+        box2 (int/float, int/float, int/float, int/float): (x, y, w, h) of second box
+
+    Output:
+        IoU (float): intersection-over-union value
+    '''
+    # Extract coordinates of the bounding boxes
+    x1, y1, w1, h1 = box1
+    x2, y2, w2, h2 = box2
+
+    # Calculate the intersection rectangle
+    intersection_x = max(x1, x2)
+    intersection_y = max(y1, y2)
+    intersection_w = max(0, min(x1 + w1, x2 + w2) - intersection_x)
+    intersection_h = max(0, min(y1 + h1, y2 + h2) - intersection_y)
+
+    # Calculate the areas of each rectangle
+    area_box1 = w1 * h1
+    area_box2 = w2 * h2
+    area_intersection = intersection_w * intersection_h
+
+    # Calculate the IoU
+    IoU = area_intersection / float(area_box1 + area_box2 - area_intersection)
+
+    return IoU
+
+def soft_nms(pano_detections_with_meta, sigma_one=0.3, sigma_two=0.6, IoU_minimum=0.5):
+    '''
+    Function:
+        Take in all panorama detections and filter out those based on the soft NMS penalty described in the paper
+    
+    Input:
+        pano_detections_with_meta (np.ndarray): Contains all panorama detections
+        sigma_one (float from 0 to 1): first parameter described in paper
+        sigma_two (float from 0 to 1): second parameter described in paper
+        IoU_minimum (float from 0 to 1): the minimum IoU needed to apply soft NMS between two detections
+
+    Output:
+        pano_detections_post_nms: an np.ndarray containing remaining panorama detections after soft NMS is applied
+    '''
+
+    pano_detections_post_nms = []
+    indices_to_remove = set()
+
+    # For each panorama detection, if it overlaps a lot with another detection (thus duplicate detections),
+    # calculate the soft NMS penalty described in paper
+    num_detections = pano_detections_with_meta.shape[0]
+    for detection_i in range(num_detections):
+        if detection_i in indices_to_remove:
+            pass
+        detection = pano_detections_with_meta[detection_i]['pano_detection']
+
+        for detection_j in range(detection_i + 1, num_detections):
+            compared_detection = pano_detections_with_meta[detection_j]['pano_detection']
+
+            # Check if the detections are for the same class
+            if detection['class_id'] == compared_detection['class_id']:
+                # Calculate the IoU of the boxes
+                IoU = calculate_IoU(detection['box'], compared_detection['box'])
+                # Check if IoU is above the minimum threshold
+                if IoU >= IoU_minimum:
+                    confidence = detection['confidence']
+                    distance = detection['dist_from_center']
+                    # Based on equation in paper
+                    rescore = confidence * np.exp(-1 * ((IoU ** 2)/sigma_one + (distance ** 2)/sigma_two))
+
+                    # Print for testing
+                    print("detection: ", detection)
+                    print("compared detection: ", compared_detection)
+                    print("rescore for detection: ", rescore)
+
+                    compared_confidence = compared_detection['confidence']
+                    compared_distance = compared_detection['dist_from_center']
+                    compared_rescore = compared_confidence * np.exp(-1 * ((IoU ** 2)/sigma_one + (compared_distance ** 2)/sigma_two))
+                    print("rescore for compared detection: ", compared_rescore)
+
+                    # If detection is better than compared, then add compared to discard pile
+                    if rescore >= compared_rescore:
+                        indices_to_remove.add(detection_j)
+                    # Otherwise, add detection to the discard pile, finish inner loop, and begin checking the next detection
+                    else:
+                        indices_to_remove.add(detection_i)
+                        break
+            
+        if detection_i not in indices_to_remove:
+            pano_detections_post_nms.append(detection) 
+
+
+    pano_detections_post_nms = np.array(pano_detections_post_nms, dtype=np.dtype([('pano_detection', object)]))
+
+    return pano_detections_post_nms
+
 def stereo_bounding_boxes_to_panorama(frame_detections_with_meta, panorama_path, stereographic_image_size, FOV):
+
     '''
     Function:
         Take a set of detections on the stereographic projections and
@@ -259,15 +359,15 @@ def stereo_bounding_boxes_to_panorama(frame_detections_with_meta, panorama_path,
     # The radius of the sphere is the same distance as the distance of the plane from the xy plane
     radius = z
 
+    pano_detections_with_meta = []
 
-    # Iterate through each frame
-    for frame_detections in frame_detections_with_meta:
-        
-        x_array = []
-        y_array = []
+    # Iterate through each frame and convert stereographic coordinates to panorama coordinates
+    for frame_index in range(frame_detections_with_meta.shape[0]):
+        frame_detections = frame_detections_with_meta[frame_index]
+
         # Get all of the detections in the frame and get the coordinates of the 4 corners of the box
-        for detection in frame_detections['image_detections']:
-            print(detection)
+        for detection_index in range(len(frame_detections['image_detections'])):
+            detection = frame_detections['image_detections'][detection_index]
             # box -> {x, y, width, height}
             box = detection['box']
             # scale is conversion factor to scale up to original image (stereographic projection plane) size
@@ -277,12 +377,6 @@ def stereo_bounding_boxes_to_panorama(frame_detections_with_meta, panorama_path,
             # Get box coordinates for x (x, x + w) and y (y, y + h) from the stereographic image
             box_coord_x = np.array([(box[0] * scale), (box[0] + box[2]) * scale])
             box_coord_y = np.array([(box[1] * scale), (box[1] + box[3]) * scale])
-            
-            
-            print("Box coordinates for x:")
-            print(box_coord_x)
-            print("Box coordinates for y:")
-            print(box_coord_y)
 
             # Get the stereographic projection plane coordinates from the stereographic image
             # Set array of x values from -W/2 to W/2 so that 0 is in center of plane
@@ -291,65 +385,102 @@ def stereo_bounding_boxes_to_panorama(frame_detections_with_meta, panorama_path,
             # Set array of y values from -H/2 to H/2 so that 0 is in center of plane
             y_plane = pano_pixel_H_range / 2 - box_coord_y * (H_step_size)
 
+            # Get distance from frame center to use for soft NMS described in paper
+            plane_center_coord_x = (x_plane[0] + x_plane[1]) / 2
+            plane_center_coord_y = (y_plane[0] + y_plane[1]) / 2
+            distance_from_frame_center = (np.sqrt(plane_center_coord_x ** 2 + plane_center_coord_y ** 2)
+                                          / np.sqrt((pano_pixel_W_range / 2) ** 2 + (pano_pixel_H_range / 2) ** 2))
+
+
             x_grid, y_grid = np.meshgrid(x_plane, y_plane, indexing="xy")
             
-            print("X grid: ", x_grid)
-            print("Y grid: ", y_grid)
-
-            x_array.append(x_grid)
-            y_array.append(y_grid)
-
-        print("Frame detection yaw: " + str(frame_detections['yaw']))
-        print("Frame detection pitch: " + str(frame_detections['pitch']))
-
-        print(x_array)
-        print(y_array)
-
-        # For each detection, draw the corresponding bounding box with the associated pixels on the panorama
-        for i in range(len(x_array)):
             # Get associated theta and phi angles
-            print("x, y, z, radius, yaw, pitch: " ,x_array[i], y_array[i], z, radius, frame_detections['yaw'], frame_detections['pitch'])
-
-            theta, phi = map_to_sphere(x_array[i], y_array[i], z, radius, np.radians(frame_detections['yaw']), np.radians(frame_detections['pitch']))
-
-            print("Phi: ", np.degrees(phi))
-            print("Theta: ", np.degrees(theta))
+            theta, phi = map_to_sphere(x_grid, y_grid, z, radius, np.radians(frame_detections['yaw']), np.radians(frame_detections['pitch']), normalizing=True)
 
             # Convert theta and phi angles to associated pixels in the panorama
             U = phi * pano_width / (2 * np.pi)
             V = theta * pano_height / np.pi
 
-            print("U: " + str(U))
-            print("V: " + str(V))
+            # Correct values for objects that are on the edge of the pano
+            if U[2] < (U[0] - pano_width / 2):
+                U[2] += pano_width
+            elif U[0] < (U[2] - pano_width / 2):
+                U[0] += pano_width
 
-            # Get label info from frame_detections
-            frame_detection = frame_detections['image_detections'][i]
-            class_id = frame_detection['class_id']
-            confidence = frame_detection['confidence']
+            if V[1] < (V[0] - pano_height / 2):
+                V[1] += pano_height
+            elif V[0] < (V[1] - pano_height / 2):
+                V[0] += pano_height
 
-            for j in range(len(U)):
+            if U[1] < U[0] or U[1] < U[2]:
+                U[1] += pano_width
+            if U[3] < U[0] or U[3] < U[2]:
+                U[3] += pano_width
+            if V[2] < V[0] or V[2] < V[1]:
+                V[2] += pano_height
+            if V[3] < V[0] or V[3] < V[1]:
+                V[2] += pano_height
 
-                # Take the smallest x and y value for top left corner and largest x and y value for the bottom right corner 
-                x_pano = min(U)
-                y_pano = min(V)
-                x_plus_w_pano = max(U)
-                y_plus_h_pano = max(V)
+            # Take the smallest x and y value for top left corner and largest x and y value for the bottom right corner
+            x_pano = min([U[0], U[2]])
+            y_pano = min([V[0], V[1]])
+            x_plus_w_pano = max([U[1], U[3]])
+            y_plus_h_pano = max([V[2], V[3]])
 
-                # Round to nearest integer for convert for pixels
-                x_pano = round(x_pano)
-                y_pano = round(y_pano)
-                x_plus_w_pano = round(x_plus_w_pano)
-                y_plus_h_pano = round(y_plus_h_pano)
+            # Round to nearest integer for convert for pixels
+            x_pano = round(x_pano)
+            y_pano = round(y_pano)
+            x_plus_w_pano = round(x_plus_w_pano)
+            y_plus_h_pano = round(y_plus_h_pano)
+            w_pano = x_plus_w_pano - x_pano
+            h_pano = y_plus_h_pano - y_pano
+            
 
-                draw_bounding_box(
-                    pano_array,
-                    class_id,
-                    confidence,
-                    x_pano,
-                    y_pano,
-                    x_plus_w_pano,
-                    y_plus_h_pano,
-                )
+            pano_detection = detection.copy()
+
+            # Box is defined as (x, y, width, height), where x and y are the top left corner of the box, keeping in line with YOLOv8 box definitions
+            pano_detection['box'] = [x_pano, y_pano, w_pano, h_pano]
+            pano_detection['dist_from_center'] = distance_from_frame_center
+
+            pano_detections_with_meta.append(pano_detection)
+    
+    pano_detections_with_meta = np.array(pano_detections_with_meta, dtype=np.dtype([('pano_detection', object)]))
+
+    print("Detections in the panorama")
+
+    # Apply Soft Non-Max Suppression on each detection
+    pano_detections_post_nms = soft_nms(pano_detections_with_meta)
+
+    # For each panorama detection, draw the corresponding bounding box with the associated pixels on the panorama
+    for frame_index in range(pano_detections_post_nms.shape[0]):
+        pano_detection = pano_detections_post_nms[frame_index]['pano_detection']
+
+        print(pano_detection)
+
+        # box -> {x, y, width, height}
+        box = pano_detection['box']
+
+        # Get bounding box information
+        x_pano = box[0]
+        x_plus_w_pano = box[0] + box[2]
+        y_pano = box[1]
+        y_plus_h_pano = box[1] + box[3]
+
+        # Get label info from detection
+        class_id = pano_detection['class_id']
+        confidence = pano_detection['confidence']
+
+        # Draw the bounding box on the panorama
+        draw_bounding_box(
+            pano_array,
+            class_id,
+            confidence,
+            x_pano,
+            y_pano,
+            x_plus_w_pano,
+            y_plus_h_pano,
+        )
+
 
     # Return the final annotated pano_array
     return pano_array
